4c9cc0e831bcb6fb62f9c3d85e71ad21476f622e
AlyssaJoyFlores <114978911+AlyssaJoyFlores@users.noreply.github.com>
2023-07-06 13:42:02 +0800

    Add files via upload

---
+++ python-act-1.py
@@ -0,0 +1,33 @@
+# Problem 1
+programmer = "Alyssa Joy Flores"
+dateCreated = "July 6, 2023"
+desc = "This is python activity"
+fname = "Alyssa Joy"
+mname = "Garcia"
+lname = "Flores"
+course = "BSIT"
+year = "3rd year"
+gender = "Female"
+civilStatus = "taken"
+nationality = "Filipino"
+motto = "Impossible is nothing"
+school = "PHINMA Araullo University"
+college = "CITE"
+
+print("BIO DATA")
+print("Programmer: ", programmer)
+print("Date Created: ", dateCreated)
+print("Description of the Program: ", desc)
+print("======================================================")
+print("First Name: ", fname)
+print("Middle Name: ", mname)
+print("Last Name: ", lname)
+print("Course: ", course)
+print("Year: ", year)
+print("Gender: ", gender)
+print("Civil Status: ", civilStatus)
+print("Nationality: ", nationality)
+print("Motto: ", motto)
+print("School: ", school)
+print("College: ", college)
+
---
+++ python-act-2.py
@@ -0,0 +1,16 @@
+# Problem 2
+
+
+num1 =5
+num2 = 8
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+
+print("SUM = {0}".format(sum))
+print("DIFFERENCE = {0}".format(difference))
+print("PRODUCT = {0}".format(product))
+print("QUOTIENT = {0}".format(quotient))
